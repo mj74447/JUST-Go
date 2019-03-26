e61d2b00864cb334a9841e98aa89aab354117efd
Mayank Joshi <mj74447@gmail.com>
2019-03-26 08:49:02 +0530

    Update StructDesign.go

--- StructDesign.go
+++ StructDesign.go
@@ -9,21 +9,21 @@ type Trade struct{
 	Price float64
 	Buy bool
 }
-
+func (t *Trade) value() float64{
+	value := float64(t.Volume) * t.Price
+	if t.Buy{
+	  value = -value
+	} 
+	return value
+}
 func main() {
-	t1:=Trade{"MSFT", 10,99.8,true}
-	fmt.Println(t1)
-	fmt.Printf("%+v\n",t1)
-	fmt.Println(t1.Symbol)
 	t2:=Trade{
 		Symbol: "MSFT",
 		Volume: 10,
 		Price: 99.98,
 		Buy: true,
 	}
-	fmt.Printf("%+v\n", t2)
-	t3 := Trade{}
-	fmt.Printf("%+v\n",t3)
+	fmt.Println(t2.value())
 	
 	}
 	
